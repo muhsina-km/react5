56efbb935756c492a170a17dc28f79c0856d481f
muhsina-km <muhsim421@gmail.com>
2023-03-23 15:56:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Movies from './components/Movies';
+import Addmovie from './components/Addmovie';
+import Addst from './components/Addst';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/mov' element={<Movies></Movies>}> </Route>
+        
+        <Route path='/add' 
+         element={<Addst data={{id:"",dir:"", lang:"",gener:"",
+          relyear:""
+        }} 
+          method="post"></Addst> } />
+      </Routes>
+  </BrowserRouter>
+  
+     </div>
   );
 }
 
